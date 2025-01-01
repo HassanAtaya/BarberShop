6a00714ca4a24d32cd2c32c67cf91f132e016640
Hassan ATAYA <hassan.ataya@evolusys.com>
2025-01-01 15:19:52 +0200

    push gitignore

--- JavaSpringBoot/BarberShop/src/main/java/com/barbershop/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.barbershop.repository;
-
-import com.barbershop.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUserNameAndPassword(String userName, String password);
-}
--- JavaSpringBoot/BarberShop/target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-insert into user (user_name, password) values ('super.admin', 'superadmin123');
